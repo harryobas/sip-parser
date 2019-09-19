54a298a5ea44342a5431a7e21bd66a838110e72e
harry obaseki <harryobas@gmail.com>
2019-09-19 18:32:28 +0200

    can parse a SIP INVITE

--- lib/sip_parser.ex
+++ lib/sip_parser.ex
@@ -13,10 +13,20 @@ defmodule SIPParser do
   @doc """
   Parses a raw SIP request to the internal representation.
   """
-  @spec parse(<< >>) :: {:ok, t} | {:error, term()}
-  def parse(_sip_message) do
-    {:ok, %__MODULE__{}}
-  end
+  @spec parse(binary()) :: {:ok, t} | {:error, term()}
+  def parse(sip_message) do
+    parsed_message =
+      String.split(sip_message, "\n\n")
+      |> clean_raw_message()
+      |> extract_headers_body_start_line()
+
+    h = List.first(parsed_message)
+    b = hd(tl(parsed_message))
+    sl = List.last(parsed_message)
+
+    {:ok, %__MODULE__{headers: h,  body: b, start_line: sl}}
+
+    end
 
   @doc """
   Gets the userpart from the URI which is present in the Request-Line. The Request-Line is the
@@ -35,4 +45,29 @@ defmodule SIPParser do
   def allowed?(_parsed_sip_message, _method) do
     false
   end
+
+  defp clean_raw_message(raw_message) do
+    message = Enum.reject raw_message, fn x -> x == "\n" end
+    message = Enum.reject message, fn x -> x == "" end
+    message
+    end
+
+  defp extract_headers_body_start_line(message) do
+    start_line = hd(message)
+    headers_body = tl(message)
+    body = Enum.reject headers_body, fn x -> String.contains? x, ":" end
+    body = Enum.join(body, ",")
+
+    headers = Enum.reject headers_body, fn x -> String.contains? x, "=" end
+    headers = Enum.map headers, fn x ->
+      n = String.split(x, ":")
+      {hd(n), hd(tl(n))}
+    end
+
+    headers = Enum.into(headers, %{})
+
+    [headers, body, start_line]
+
+  end
+
 end
